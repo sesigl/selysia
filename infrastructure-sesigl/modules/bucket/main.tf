7182d9d206f4d4a28a560a88b651c81f3da92a70
Sebastian Sigl <sebastian.sigl@adevinta.com>
2023-04-10 07:14:03 +0200

    Replace selysium with selysia

--- infrastructure-sesigl/modules/bucket/main.tf
+++ infrastructure-sesigl/modules/bucket/main.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket_cors_configuration" "example" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
-    allowed_origins = ["https://www.selysium.com"]
+    allowed_origins = ["https://www.selysia.com"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
